10cbeb016fd49fdd44e4f6d811d9ddf451947beb
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-30 16:06:55 +0200

    [TICKET]

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  name: 'MyApp',
+}
 
 </script>
 
-<template></template>
+<template>
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+    <AppMain />
+  </main>
+
+  <footer>
+    <AppFooter />
+  </footer>
+</template>
 
 <style></style>
