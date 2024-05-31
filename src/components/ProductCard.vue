814a46556bf5083c1280e1929c1b64516969cb77
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-31 18:05:20 +0200

    [UPD] Creato componente card e aggiunta la props

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default {
+    name: 'ProductCard',
+    props: ['element']
+}
+
+</script>
+
+
+<template>
+        <div class="prodotto position-relative">
+        <img class="img-prodotto" :src="`../../public/img/${element.frontImage}`" alt="prodotto-1">
+        <img class="img-prodotto-secondo" :src="`../../public/img/${element.backImage}`" alt="prodotto-1-bis">
+        <input class="cuore position-absolute" type="button" value="&hearts;">
+        <div class="etichette">
+            <span class="percentuale-sconto" v-for="etichetta in element.badges" v-show="etichetta.type === 'discount'">
+                {{ etichetta.value }}
+            </span>
+        <span class="sostenibilità" v-for="etichetta in element.badges" v-show="etichetta.type === 'tag'">
+            {{ etichetta.value }}
+        </span>
+        </div>
+    </div>
+    <div class="testo-prodotto">
+        <span>{{element.brand}}</span>
+        <h3>{{element.name}}</h3>
+        <span class="prezzo-scontato" >{{element.price}}&euro;</span>  
+        <span class="prezzo-originale">{{}}</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/styles/main.scss';
+</style>
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,5 +1,6 @@
 <script>
 import products from '../data/db.json';
+import ProductCard from './ProductCard.vue';
 
 
 export default{
@@ -7,13 +8,12 @@ export default{
     data(){
         return{
             products: products.products,
-
-            sconto: products.badges
-
         }
     },
 
-
+    components:{
+        ProductCard,
+    }
 
 };
 
@@ -22,28 +22,11 @@ export default{
 <template>
 <div class="col33" v-for="(element, index) in products" :key="index">
 
-    <div class="prodotto position-relative">
-        <img class="img-prodotto" :src="`../../public/img/${element.frontImage}`" alt="prodotto-1">
-        <img class="img-prodotto-secondo" :src="`../../public/img/${element.backImage}`" alt="prodotto-1-bis">
-        <input class="cuore position-absolute" type="button" value="&hearts;">
-        <div class="etichette">
-            <span class="percentuale-sconto" v-for="etichetta in element.badges" v-show="etichetta.type === 'discount'">
-                {{ etichetta.value }}
-            </span>
-        <span class="sostenibilità" v-for="etichetta in element.badges" v-show="etichetta.type === 'tag'">
-            {{ etichetta.value }}
-        </span>
-        </div>
-    </div>
-    <div class="testo-prodotto">
-        <span>{{element.brand}}</span>
-        <h3>{{element.name}}</h3>
-        <span class="prezzo-scontato" >{{element.price}}&euro;</span>  
-        <span class="prezzo-originale">{{}}</span>
-    </div>
-</div>
+<ProductCard :element="element"/>
 
+</div>
 </template>
+
 
 <style lang="scss" scoped>
 @use '../assets/styles/main.scss';
